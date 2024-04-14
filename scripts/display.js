856f07a994b00c2e2ff497d9e7c4597cb368c402
alexander-russell <alexander.russell4@pm.me>
2024-04-14 21:41:30 +1000

    Push version 1 of index page with search

--- scripts/display.js
+++ scripts/display.js
@@ -161,6 +161,13 @@ function displayRecipeHeaderIcons(recipe) {
         iconsListElement.childNodes[0].remove();
     }
 
+    // Add a home icon to go to home page
+    homeIcon = createIconHome();
+    iconsListElement.appendChild(homeIcon);
+    homeIcon.addEventListener("click", () => {
+        location.href = "/";
+    });
+
     // If image: add camera icon
     if (recipe.Image != "") {
         // Add element
@@ -249,6 +256,10 @@ function createIconClock() {
 
 function createIconList() {
     return createIcon("list-icon", "Browse other recipes", "0 0 512 512", "M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z")
+}
+
+function createIconHome() {
+    return createIcon("home-icon", "Go to home page", "0 0 576 512", "M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z");
 }
 
 function displayRecipeAttributes(recipe) {
--- scripts/index.js
+++ scripts/index.js
@@ -1,165 +1,59 @@
-let allRecipes;
-let searchNameField = document.getElementById('name');
-let searchTypeField = document.getElementById('type');
-let searchCategoryField = document.getElementById('category');
+let recipeBook;
+let recipesLoaded = false;
+let searchInputted = false;
 
-//for data fetching
-// async function populate() {
-//     // const requestURL = "https://raw.githubusercontent.com/alexander-russell37/hosting/main/Recipes.json";
-//     const requestURL = "../data/Recipes.json"
-//     const request = new Request(requestURL);
-//     const response = await fetch(request);
-//     allRecipes = await response.json();
-//     populateRecipes(allRecipes, '', '', '');
-// }
-
-fetch("../Site/data/Selection.txt")
-    .then((response) => response.text())
-    .then((data) => {
-        SelectionName = data
-        console.log(data)
-    })
-
-fetch("../Recipes.json")
+// Get data
+fetch("RecipesCost.json")
     .then((response) => response.json())
     .then((data) => {
-        populateRecipes(data, '', '', '');
-        allRecipes = data;
-        // start();
+        recipeBook = data;
+        recipesLoaded = true;
+        start();
     })
-    .catch(err => console.error(err));
-
+    .catch(error => console.error(error));
 
 function start() {
-    for (let i = 0; i < allRecipes.length; i++) {
-        console.log(allRecipes[i].name);
-        myImg = document.createElement("img")
-        myImg.setAttribute("src", `../Gallery/${allRecipes[i].name}.jpg`);
-        myImg.setAttribute("width", "10");
-        document.body.appendChild(myImg)
+    if (recipesLoaded) {
+        // Add form event listener
+        const searchInputElement = document.querySelector("input#name");
+        searchInputElement.addEventListener("input", (event) => {
+            searchInputted = true;
+            populateResults(event.target.value);
+        });
     }
 }
 
-function imageExists(url) {
-    return new Promise(resolve => {
-        var img = new Image()
-        img.addEventListener('load', () => resolve(true))
-        img.addEventListener('error', () => resolve(false))
-        img.src = url
-    })
-}
+//
+function populateResults(query) {
+    if (searchInputted) {
+        // Get element
+        searchResultsList = document.querySelector(".search-results");
+        
+        // Get matching results
+        let results = recipeBook.Recipes.filter((currentValue) => {
+            const success =
+            currentValue.Name.toLowerCase().includes(query.toLowerCase())
+            return success;
+        });
+        console.log(results);
 
-
-// async function doThing() {
-
-//     let requestURL2 = "../data/ex.txt";
-//     let Brequest = new Request(requestURL2)
-//     const response = await fetch(Brequest);
-//     let textblah = await response.text();
-//     console.log(textblah);
-// }
-
-// doThing();
-
-// fetch("../data/ex.txt")
-//     .then((response) => response.text())
-//     .then((data) => console.log(data.split("\n")[2]));
-
-//for presenting
-function populateRecipes(allRecipes, searchName, searchType, searchCategory) {
-    //clear options list and recipe sections
-    // const table = document.querySelector('.movie-table');
-    const recipesDisplayed = document.querySelectorAll('.recipe');
-    for (const recipeDisplayed of recipesDisplayed) {
-        recipeDisplayed.remove();
-    }
-    document.querySelector(".recipe-options").innerHTML = '';
-    //filter
-    let recipes = allRecipes.filter((currentValue) => {
-        const success =
-            currentValue.name.toLowerCase().includes(searchName.toLowerCase()) &&
-            currentValue.type.toLowerCase().includes(searchType.toLowerCase()) &&
-            currentValue.category.toLowerCase().includes(searchCategory.toLowerCase());
-        return success;
-    });
-
-    //format and display options
-    optionsList = document.querySelector(".recipe-options")
-    for (let i = 0; i < recipes.length; i++) {
-        optionsListItem = document.createElement("li");
-        optionsListItemLink = document.createElement("a");
-        optionsListItemLink.setAttribute("href", `#${recipes[i].name.toLowerCase().replace(" ", "-")}`)
-        optionsListItemLink.textContent = recipes[i].name;
-        // optionsListItem.textContent = recipes[i].name;
-        optionsListItem.appendChild(optionsListItemLink);
-        optionsList.appendChild(optionsListItem);
-    }
-
-    //format and display full recipes
-    const page = document.querySelector(".page");
-    for (let i = 0; i < recipes.length; i++) {
-        //create section
-        recipeSection = document.createElement("section");
-        recipeSection.setAttribute('class', 'recipe');
-        recipeSection.setAttribute('id', recipes[i].name.toLowerCase().replace(' ', '-'))
-        //create title
-        recipeTitle = document.createElement("h2");
-        recipeTitle.textContent = recipes[i].name;
-        //display image
-        // TODO check if it exists before display
-        recipeImage = document.createElement("img");
-        recipeImage.setAttribute("src", `../Gallery/${allRecipes[i].name}.jpg`);
-        recipeImage.setAttribute("width", "100");
-        //create note
-        recipeNote = document.createElement("p");
-        recipeNote.innerHTML = `-----<br>${recipes[i].note}<br>-----`;
-        //create ingredients heading
-        recipeIngredientsHeading = document.createElement("h3");
-        recipeIngredientsHeading.textContent = "Ingredients:"
-        //create ingredients list
-        recipeIngredientsList = document.createElement("ul");
-        for (item of recipes[i].items) {
-            recipeIngredientsListItem = document.createElement("li");
-            recipeIngredientsListItem.textContent = `${item.thing} (${item.quantity})`;
-            //append because in loop
-            recipeIngredientsList.appendChild(recipeIngredientsListItem);
+        // Clear existing
+        while (searchResultsList.hasChildNodes()) {
+            searchResultsList.childNodes[0].remove();
         }
-        //create steps heading
-        recipeStepsHeading = document.createElement("h3");
-        recipeStepsHeading.textContent = "Steps:"
-        //create steps list
-        recipeStepsList = document.createElement("ol");
-        for (step of recipes[i].steps) {
-            recipeStepsListItem = document.createElement("li");
-            recipeStepsListItem.textContent = step;
-            //append because in loop
-            recipeStepsList.appendChild(recipeStepsListItem);
+
+        // Add new results
+        for (const result of results) {
+            // Create list item
+            searchResultElement = document.createElement("li");
+            searchResultElement.classList.add("search-result");
+            searchResultsList.appendChild(searchResultElement);
+
+            // Create link
+            searchResultLinkElement = document.createElement("a");
+            searchResultLinkElement.href = `display?recipe=${result.Name}`;
+            searchResultLinkElement.textContent = result.Name;
+            searchResultElement.appendChild(searchResultLinkElement);
         }
-        //append bits
-        recipeSection.appendChild(recipeTitle);
-        recipeSection.appendChild(recipeImage);
-        recipeSection.appendChild(recipeNote);
-        recipeSection.appendChild(recipeIngredientsHeading);
-        recipeSection.appendChild(recipeIngredientsList);
-        recipeSection.appendChild(recipeStepsHeading);
-        recipeSection.appendChild(recipeStepsList);
-        page.insertBefore(recipeSection, page.childNodes[10]);
     }
 }
-
-//initial presentation
-// populate();
-
-//updating based on field changes
-searchNameField.addEventListener('keydown', () => {
-    populateRecipes(allRecipes, searchNameField.value, searchTypeField.value, searchCategoryField.value);
-});
-
-searchTypeField.addEventListener('keydown', () => {
-    populateRecipes(allRecipes, searchNameField.value, searchTypeField.value, searchCategoryField.value);
-});
-
-searchCategoryField.addEventListener('keydown', () => {
-    populateRecipes(allRecipes, searchNameField.value, searchTypeField.value, searchCategoryField.value);
-});
-
