a59af9385a0295fbbf74d4678dd800c5af96a87e
alexander-russell <alexander.russell4@pm.me>
2024-11-19 11:34:00 +1100

    Focus one category using URL parameter

--- scripts/contents.js
+++ scripts/contents.js
@@ -5,24 +5,29 @@ fetchRecipeBook()
 // Start function runs when recipeBook is loaded from file
 async function start(recipeBook) {
     const contentsElement = document.querySelector(".contents")
+
+    // Retrieve URL params, obliterate them
+    urlParams = new URLSearchParams(document.location.search);
+    history.pushState({}, "", window.location.href.split('?')[0])
+
     // Filter to active recipes and display
     const activeRecipes = filterActiveRecipes(recipeBook.Recipes);
-    displayContents(activeRecipes, contentsElement);
+    displayContents(activeRecipes, contentsElement, urlParams);
 }
 
 // Display contents using recipe list
-function displayContents(recipes, contentsElement) {
+function displayContents(recipes, contentsElement, urlParams) {
     // Display each type of recipe
     const allTypes = recipes.map((recipe) => recipe.Type)
     const types = [...new Set(allTypes)]
     for (const type of types.sort()) {
         // Filter relevant recipes and use them
         const subRecipes = recipes.filter((recipe) => recipe.Type == type)
-        displayType(type, subRecipes, contentsElement)
+        displayType(type, subRecipes, contentsElement, urlParams)
     }
 }
 
-function displayType(type, recipes, contentsElement) {
+function displayType(type, recipes, contentsElement, urlParams) {
     // Display type information
     const heading = document.createElement("h2");
     heading.textContent = type;
@@ -34,11 +39,11 @@ function displayType(type, recipes, contentsElement) {
     for (const category of categories.sort()) {
         // Filter relevant recipes and use them
         const subRecipes = recipes.filter((recipe) => recipe.Category == category)
-        displayCategory(category, subRecipes, contentsElement);
+        displayCategory(category, subRecipes, contentsElement, urlParams);
     }
 }
 
-function displayCategory(category, recipes, contentsElement) {
+function displayCategory(category, recipes, contentsElement, urlParams) {
     // Display category information
     const details = document.createElement("details");
     contentsElement.appendChild(details);
@@ -53,16 +58,17 @@ function displayCategory(category, recipes, contentsElement) {
     count.textContent = recipes.length;
     summary.appendChild(count);
 
-    // Configure category dropdown menu
-
     // Populate category dropdown menu with recipes
     populateCategoryDropdown(recipes, details);
+
+    // Configure category dropdown menu
+    configureDropdown(details, category, urlParams);
 }
 
 function populateCategoryDropdown(recipes, details) {
     const list = document.createElement("ul");
     details.appendChild(list);
-    for (recipe of recipes) {    
+    for (recipe of recipes) {
         const item = document.createElement("li");
         list.appendChild(item);
         const link = document.createElement("a");
@@ -72,11 +78,19 @@ function populateCategoryDropdown(recipes, details) {
     }
 }
 
+function configureDropdown(details, category, urlParams) {
+    if (urlParams.get("focus") == category) {
+        // Open the details pane and mark it for focus styling
+        details.setAttribute("open", "");
+        details.classList.add("focus");
+    }
+}
+
 function toggleList() {
     // Determine which control is visible, and therefore what action to perform
     const visibleControl = document.querySelector(".list-control:not(.hidden)")
     expand = visibleControl.querySelector("#expand-icon") != null
-    
+
     // Switch visible control (either expand or minimise)
     const listControls = document.querySelectorAll(".list-control");
     listControls.forEach(listControl => listControl.classList.toggle("hidden"));
